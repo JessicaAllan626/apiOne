5b8e1737be21b0eff97cf4e800a213eac597cfbd
Jessica Allan <JessicaAllan626@gmail.com>
2021-10-05 20:00:02 -0400

    added changes

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ const retrieveLocation = async(id)=>{
     let data = await response.json();
     let location = data.results[id];
     let name = location.name;
-    alert(`The location you picked is ${name}`);
+    alert(`Quick!! Head to ${name} to catch up to Ash!!`);
 }
 
 retrieveLocation().catch(error => console.log(error));
